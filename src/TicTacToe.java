5f26e133d7761ba9f482c70b7ade96899dbcad95
roaaeliwah <roaa.m.elsayed@gmail.com>
2025-10-04 01:11:33 +0300

    Added upper panel

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -7,15 +7,28 @@ public class TicTacToe {
     int boardWidth = 500;
     int boardHeight = 550;
 
-    JFrame frame = new JFrame();
+    JFrame frame = new JFrame("Tic-Tac-Toe");
+    JLabel textLabel = new JLabel();
+    JPanel textPanel = new JPanel();
 
     TicTacToe() {
         frame.setVisible(true);
         frame.setSize(boardWidth, boardHeight);
         frame.setLocationRelativeTo(null);
-        //frame.setResizable(false);
+        frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new BorderLayout());
+
+        textLabel.setFont(new Font("Times New Roman", Font.BOLD, 50));
+        textLabel.setHorizontalAlignment(JLabel.CENTER);
+        textLabel.setBackground(Color.pink);
+        textLabel.setForeground(Color.white);
+        textLabel.setText("Tic-Tac-Toe");
+        textLabel.setOpaque(true);
+
+        textPanel.setLayout(new BorderLayout());
+        textPanel.add(textLabel);
+        frame.add(textPanel, BorderLayout.NORTH);
 
     }
 
